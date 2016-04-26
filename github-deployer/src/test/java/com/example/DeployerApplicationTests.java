1c7c70d5b797db0c25c9b7f4f63554ba03e7b694
Dave Syer <dsyer@pivotal.io>
2016-04-26 14:48:30 +0100

    Add a login page to deployer

--- github-deployer/src/test/java/com/example/DeployerApplicationTests.java
+++ github-deployer/src/test/java/com/example/DeployerApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DeployerApplication.class)
-@WebIntegrationTest
+@WebIntegrationTest(randomPort=true)
 public class DeployerApplicationTests {
 
 	@Test
